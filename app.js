92c2ab925ff027543ed8ec59e6d8d6e4f43cfbfb
TinaLacatis <danatina.dt@gmail.com>
2022-10-10 12:05:41 +0200

    add envirroment vars and new node.js packages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require ("mongoose")
+const encrypt = require("mongoose-encryption")
 
 const app = express();
 
@@ -10,10 +12,12 @@ app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
